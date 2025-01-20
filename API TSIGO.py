8cd907037ad2a639253bad81597ee0d090b41c08
Matheus Rodrigues <Matheusrodd17@gmail.com>
2025-01-20 08:59:34 -0300

    novo codigo para API TSIGO, está funcional, puxando 15 dias anterior desde o data_atual

--- API TSIGO.py
+++ API TSIGO.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+from datetime import datetime, timedelta
 
 url_login = "https://api.getsec.com.br/login/entrar/"
 dados_login = {
@@ -11,15 +12,15 @@ response_login = requests.post(url_login, json=dados_login)
 
 if response_login.status_code == 200:
     token = response_login.json().get("token")
-    
+
     if token:
         url_listar_veiculos = "https://api.getsec.com.br/veiculo/listar/"
         data_listar = {
             "token": token
         }
-        
-        response_listar = requests.post(url_listar_veiculos, json=data_listar)  
-        
+
+        response_listar = requests.post(url_listar_veiculos, json=data_listar)
+
         if response_listar.status_code == 200:
             lista_veiculos = response_listar.json()
             lista_ids_veiculos = [veiculo['id'] for veiculo in lista_veiculos]
@@ -27,13 +28,15 @@ if response_login.status_code == 200:
 
             url_posicao = "https://api.getsec.com.br/posicao/listar/"
 
-
+            # Calcula os últimos 15 dias
+            data_hora_fim = datetime.now()
+            data_hora_inicio = data_hora_fim - timedelta(days=15)
 
             dados_posicao_base = {
                 "token": token,
-                "id_veiculo": "2878",
-                "data_hora_inicio": "2024-11-15-03:00:00",
-                "data_hora_fim": "2099-11-20-03:00:00"
+                "id_veiculo": "",
+                "data_hora_inicio": data_hora_inicio.strftime("%Y-%m-%d %H:%M:%S"),
+                "data_hora_fim": data_hora_fim.strftime("%Y-%m-%d %H:%M:%S")
             }
 
             all_dataframes = []
@@ -41,13 +44,13 @@ if response_login.status_code == 200:
             for id_veiculo in lista_ids_veiculos:
                 dados_posicao = dados_posicao_base.copy()
                 dados_posicao['id_veiculo'] = id_veiculo
-                
+
                 response_posicao = requests.post(url_posicao, json=dados_posicao)
-                
+
                 if response_posicao.status_code == 200:
                     resposta_posicao = response_posicao.json()
                     print(f"Resposta da API para o veículo {id_veiculo}: {resposta_posicao}")
-                    
+
                     if resposta_posicao.get('codigo') != 1:
                         if 'data' in resposta_posicao and isinstance(resposta_posicao['data'], list):
                             df = pd.json_normalize(resposta_posicao['data'])
@@ -58,11 +61,11 @@ if response_login.status_code == 200:
                         print(f'Erro na API para o veículo {id_veiculo}:', resposta_posicao.get('mensagem'))
                 else:
                     print(f'Erro ao obter informações para o veículo {id_veiculo}:', response_posicao.status_code, response_posicao.text)
-    
+
             if all_dataframes:
                 final_df = pd.concat(all_dataframes, ignore_index=True)
                 print(final_df)
-            else:   
+            else:
                 print('Nenhum dado foi coletado.')
         else:
             print("Erro ao listar veículos:", response_listar.status_code, response_listar.text)
@@ -70,4 +73,3 @@ if response_login.status_code == 200:
         print("Erro: Token não encontrado na resposta de login.")
 else:
     print("Erro ao fazer login:", response_login.status_code, response_login.text)
-
